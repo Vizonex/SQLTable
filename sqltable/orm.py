c59fa73cedb0a08917b80902b457910ebc01e5e3
Vizonex <114684698+Vizonex@users.noreply.github.com>
2025-03-02 21:53:39 -0600

    Fix Mapped Variables for Json Decoding

--- sqltable/orm.py
+++ sqltable/orm.py
@@ -1,2 +1,14 @@
-"""Import helper for sqlalchmey made only for convenince"""
-from sqlalchemy.orm import Mapped, mapped_column
+"""Import helper for sqlalchmey made for helping the json decoder with Mapped Variables"""
+from sqlalchemy.orm import Mapped as _Mapped, mapped_column
+from typing import TypeVar, Annotated, TYPE_CHECKING
+
+T = TypeVar("T")
+
+# HACK: msgspec doesn't like mapped variables for encoding so we
+# need to do some shady stuff to Force it...
+# Because otherwise we get msgspec.ValidationError: Expected `Mapped`, got `str` - at `$.[blah blah blah]`
+if not TYPE_CHECKING:
+    Mapped = Annotated[T, _Mapped[T]]
+else:
+    Mapped = _Mapped
+
