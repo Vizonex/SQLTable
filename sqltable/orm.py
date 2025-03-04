01b597012f86cb097631bd7eb0efd264aceebbcd
Vizonex <114684698+Vizonex@users.noreply.github.com>
2025-03-03 22:51:43 -0600

    Revert Because Json Decoder Mixin was fixed.

--- sqltable/orm.py
+++ sqltable/orm.py
@@ -1,14 +1,2 @@
 """Import helper for sqlalchmey made for helping the json decoder with Mapped Variables"""
-from sqlalchemy.orm import Mapped as _Mapped, mapped_column
-from typing import TypeVar, Annotated, TYPE_CHECKING
-
-T = TypeVar("T")
-
-# HACK: msgspec doesn't like mapped variables for encoding so we
-# need to do some shady stuff to Force it...
-# Because otherwise we get msgspec.ValidationError: Expected `Mapped`, got `str` - at `$.[blah blah blah]`
-if not TYPE_CHECKING:
-    Mapped = Annotated[T, _Mapped[T]]
-else:
-    Mapped = _Mapped
-
+from sqlalchemy.orm import Mapped, mapped_column
